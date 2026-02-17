777d7559d82c99166db732dd57006b64c7653962
Akil <113962713+akil4@users.noreply.github.com>
2026-02-17 09:18:17 +0530

    foundations/11

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const isDivisibleByFour = year % 4 === 0;
+    const isDivisibleByHundred = year % 100 === 0;
+    const isDivisibleByFourHundred = year % 400 === 0;
 
+    return (isDivisibleByFour && (!isDivisibleByHundred || isDivisibleByFourHundred)) ? true : false;
+    
 };
 
 // Do not edit below this line
