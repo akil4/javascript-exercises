32dd3ba96e9e1bc421becbac55d6da748f3b4147
Akil <113962713+akil4@users.noreply.github.com>
2026-07-22 19:10:23 +0530

    complete foundations/13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.toLowerCase().trim().match(/[a-zA-Z0-9]/g);
 
+    let start = 0;
+    let end = string.length - 1;
+
+    while (start < end) {
+        if (string[start] === string[end]) {
+            start++;
+            end--;
+        }
+        else if (string[start] !== string[end]) {
+            return false;
+        }
+    }
+
+    return true;
 };
 
 // Do not edit below this line
