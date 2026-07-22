84712e5e53b8a780ddcd5a69a27062f27c4c7779
Akil <113962713+akil4@users.noreply.github.com>
2026-07-22 18:29:34 +0530

    complete foundations/12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((sum, element) => sum + element, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product, element) => product * element, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(N) {
+	if (N <= 1) return 1;
+  return N * factorial(N - 1);
 };
 
 // Do not edit below this line
