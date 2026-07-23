a3ebaa774c853a4963c73618b41505ab3845ac67
Akil <113962713+akil4@users.noreply.github.com>
2026-07-23 11:46:26 +0530

    complete foundation/16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    // Add age in object
+    const agedObject = array.map(item => {
+        let age = 0;
 
+        if (item.yearOfDeath) age = item.yearOfDeath - item.yearOfBirth;
+        else age = new Date().getFullYear() - item.yearOfBirth;
+
+        return {name: item.name, age: age};
+    });
+    // Sort array of objects in Descending order by age
+    const sortedArray = agedObject.sort((a, b) => b.age - a.age);
+    // Return object
+    return sortedArray[0];
 };
 
 // Do not edit below this line
